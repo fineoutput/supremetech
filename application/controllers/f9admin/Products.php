8dd00b9c22964cec2bec0a3cb7664a02a0d794fd
taruna01 <82155446+taruna01@users.noreply.github.com>
2021-09-03 13:22:49 +0530

    admin

--- application/controllers/f9admin/Products.php
+++ application/controllers/f9admin/Products.php
@@ -40,6 +40,38 @@ function __construct()
 
                    }
 
+ public function view_inventory($id){
+
+									 								 if(!empty($this->session->userdata('admin_data'))){
+
+
+									 									 $data['user_name']=$this->load->get_var('user_name');
+
+									 									 // echo SITE_NAME;
+									 									 // echo $this->session->userdata('image');
+									 									 // echo $this->session->userdata('position');
+									 									 // exit;
+
+									 															$this->db->select('*');
+									 									 $this->db->from('tbl_inventory');
+									 									 $this->db->where('product_id',$id);
+									 									 $data['inventory_data']= $this->db->get();
+
+
+									 									 $this->load->view('admin/common/header_view',$data);
+									 									 $this->load->view('admin/products/view_inventory');
+									 									 $this->load->view('admin/common/footer_view');
+
+									 							 }
+									 							 else{
+
+									 									redirect("login/admin_login","refresh");
+									 							 }
+
+									 							 }
+
+
+
 public function add_products(){
 
                  if(!empty($this->session->userdata('admin_data'))){
@@ -99,9 +131,45 @@ public function add_products(){
                 $model_no=$this->input->post('model_no');
                 $subcategory_id=$this->input->post('subcategory_id');
 
-								$productsimg = time() . '_' . $_FILES["slider_image"]["name"];
-								$liciense_tmp_name = $_FILES["slider_image"]["tmp_name"];
-								$error = $_FILES["slider_image"]["error"];
+								// $img1='image';
+								//
+								//             $file_check=($_FILES['image']['error']);
+								//             if($file_check!=4){
+								//           	$image_upload_folder = FCPATH . "assets/uploads/products/";
+								//   						if (!file_exists($image_upload_folder))
+								//   						{
+								//   							mkdir($image_upload_folder, DIR_WRITE_MODE, true);
+								//   						}
+								//   						$new_file_name="product".date("Ymdhms");
+								//   						$this->upload_config = array(
+								//   								'upload_path'   => $image_upload_folder,
+								//   								'file_name' => $new_file_name,
+								//   								'allowed_types' =>'jpg|jpeg|png',
+								//   								'max_size'      => 25000
+								//   						);
+								//   						$this->upload->initialize($this->upload_config);
+								//   						if (!$this->upload->do_upload($img1))
+								//   						{
+								//   							$upload_error = $this->upload->display_errors();
+								//   							// echo json_encode($upload_error);
+								//   							echo $upload_error;
+								//   						}
+								//   						else
+								//   						{
+								//
+								//   							$file_info = $this->upload->data();
+								//
+								//   							$videoNAmePath = "assets/uploads/products/".$new_file_name.$file_info['file_ext'];
+								//   							$file_info['new_name']=$videoNAmePath;
+								//   							// $this->step6_model->updateappIconImage($imageNAmePath,$appInfoId);
+								//   							$nnnn=$file_info['file_name'];
+								//   							// echo json_encode($file_info);
+								//   						}
+								//             }
+
+								$productsimg = time() . '_' . $_FILES["image"]["name"];
+								$liciense_tmp_name = $_FILES["image"]["tmp_name"];
+								$error = $_FILES["image"]["error"];
 								$liciense_path = 'assets/admin/products/' . $productsimg;
 								move_uploaded_file($liciense_tmp_name, $liciense_path);
 								$prdctimage = $liciense_path;
@@ -217,6 +285,198 @@ $this->session->set_flashdata('emessage','Please insert some data, No data avail
 
           }
 
+public function add_inventory($id){
+
+					                 if(!empty($this->session->userdata('admin_data'))){
+
+
+					                   $data['user_name']=$this->load->get_var('user_name');
+
+														 $data['productt_id'] = $id;
+
+					                   // echo SITE_NAME;
+					                   // echo $this->session->userdata('image');
+					                   // echo $this->session->userdata('position');
+					                   // exit;
+					                  //           $this->db->select('*');
+					                  // $this->db->from('tbl_subcategory');
+					                  // //$this->db->where('id',$usr);
+					                  // $data['iven_data']= $this->db->get();
+
+
+					                   $this->load->view('admin/common/header_view',$data);
+					                   $this->load->view('admin/products/add_inventory');
+					                   $this->load->view('admin/common/footer_view');
+
+					               }
+					               else{
+
+					                  redirect("login/admin_login","refresh");
+					               }
+
+					               }
+
+public function add_inventory_data($t,$iw="",$product_id)
+
+					              {
+
+					                if(!empty($this->session->userdata('admin_data'))){
+
+
+					          	$this->load->helper(array('form', 'url'));
+					            $this->load->library('form_validation');
+					            $this->load->helper('security');
+					            if($this->input->post())
+					            {
+					              // print_r($this->input->post());
+					              // exit;
+					              $this->form_validation->set_rules('stocks', 'stocks', 'required|xss_clean|trim');
+					              $this->form_validation->set_rules('model_no', 'model_no', 'required|xss_clean|trim');
+					              $this->form_validation->set_rules('colour', 'colour', 'required|xss_clean|trim');
+
+					              if($this->form_validation->run()== TRUE)
+					              {
+					                $stocks=$this->input->post('stocks');
+					                $model_no=$this->input->post('model_no');
+													$colour=$this->input->post('colour');
+													$product_idds = $product_id;
+
+					                  $ip = $this->input->ip_address();
+					          date_default_timezone_set("Asia/Calcutta");
+					                  $cur_date=date("Y-m-d H:i:s");
+
+					                  $addedby=$this->session->userdata('admin_id');
+
+					          $typ=base64_decode($t);
+					          if($typ==1){
+
+					          $data_insert = array('stocks'=>$stocks,
+					                    'model_no'=>$model_no,
+															'colour'=>$colour,
+															'product_id'=>$product_idds,
+															'ip'=>$ip,
+					                    'added_by' =>$addedby,
+					                    'is_active' =>1,
+					                    'date'=>$cur_date
+
+					                    );
+
+
+					          $last_id=$this->base_model->insert_table("tbl_inventory",$data_insert,1) ;
+
+					          }
+					          if($typ==2){
+
+					   $idw=base64_decode($iw);
+
+					// $this->db->select('*');
+					//     $this->db->from('tbl_minor_category');
+					//    $this->db->where('name',$name);
+					//     $damm= $this->db->get();
+					//    foreach($damm->result() as $da) {
+					//      $uid=$da->id;
+					// if($uid==$idw)
+					// {
+					//
+					//  }
+					// else{
+					//    echo "Multiple Entry of Same Name";
+					//       exit;
+					//  }
+					//     }
+
+					          $data_insert = array('stocks'=>$stocks,
+					                    'model_no'=>$model_no,
+															'colour'=>$colour,
+					                    );
+
+
+
+
+					          	$this->db->where('id', $idw);
+					            $last_id=$this->db->update('tbl_inventory', $data_insert);
+
+					          }
+
+
+					                              if($last_id!=0){
+
+					                              $this->session->set_flashdata('smessage','Data inserted successfully');
+
+					                              redirect("dcadmin/products/view_products","refresh");
+
+					                                      }
+
+					                                      else
+
+					                                      {
+
+					                                	 $this->session->set_flashdata('emessage','Sorry error occured');
+					                              		   redirect($_SERVER['HTTP_REFERER']);
+
+
+					                                      }
+
+
+					              }
+					            else{
+
+					$this->session->set_flashdata('emessage',validation_errors());
+					     redirect($_SERVER['HTTP_REFERER']);
+
+					            }
+
+					            }
+					          else{
+
+					$this->session->set_flashdata('emessage','Please insert some data, No data available');
+					     redirect($_SERVER['HTTP_REFERER']);
+
+					          }
+					          }
+					          else{
+
+								redirect("login/admin_login","refresh");
+
+
+					          }
+
+					          }
+
+										public function update_inventory($idd){
+
+																		 if(!empty($this->session->userdata('admin_data'))){
+
+
+																			 $data['user_name']=$this->load->get_var('user_name');
+
+																			 // echo SITE_NAME;
+																			 // echo $this->session->userdata('image');
+																			 // echo $this->session->userdata('position');
+																			 // exit;
+
+																				$id=base64_decode($idd);
+																			 $data['id']=$idd;
+
+																			 $this->db->select('*');
+																									 $this->db->from('tbl_inventory');
+																									 $this->db->where('id',$id);
+																									 $dsa= $this->db->get();
+																									 $data['inventory']=$dsa->row();
+
+
+																			 $this->load->view('admin/common/header_view',$data);
+																			 $this->load->view('admin/products/update_inventory');
+																			 $this->load->view('admin/common/footer_view');
+
+																	 }
+																	 else{
+
+																			redirect("login/admin_login","refresh");
+																	 }
+
+																	 }
+
 
 					public function update_products($idd){
 
@@ -256,6 +516,49 @@ $this->session->set_flashdata('emessage','Please insert some data, No data avail
 					               }
 
 					               }
+
+public function delete_inventory($idd){
+
+
+
+												        if(!empty($this->session->userdata('admin_data'))){
+
+
+												          $data['user_name']=$this->load->get_var('user_name');
+
+												          // echo SITE_NAME;
+												          // echo $this->session->userdata('image');
+												          // echo $this->session->userdata('position');
+												          // exit;
+												                  									 $id=base64_decode($idd);
+
+												         if($this->load->get_var('position')=="Super Admin"){
+
+												             $zapak=$this->db->delete('tbl_inventory', array('id' => $id));
+												             if($zapak!=0){
+																			 redirect("dcadmin/products/view_products","refresh");
+												              }
+												               else
+												              {
+												                echo "Error";
+												                 exit;
+												               }
+												           		}
+												                else{
+												              			$data['e']="Sorry You Don't Have Permission To Delete Anything.";
+												             				// exit;
+												                    $this->load->view('errors/error500admin',$data);
+												               }
+
+												              }
+												              else{
+
+												                  $this->load->view('admin/login/index');
+												              }
+
+												              }
+
+
 
 public function delete_products($idd){
 
@@ -363,6 +666,67 @@ public function updateproductsStatus($idd,$t){
 
        }
 
+			 public function updateproductsinventoryStatus($idd,$t){
 
+			          if(!empty($this->session->userdata('admin_data'))){
+
+
+			            $data['user_name']=$this->load->get_var('user_name');
+
+			            // echo SITE_NAME;
+			            // echo $this->session->userdata('image');
+			            // echo $this->session->userdata('position');
+			            // exit;
+			            $id=base64_decode($idd);
+
+			            if($t=="active"){
+
+			              $data_update = array(
+			          'is_active'=>1
+
+			          );
+
+			          $this->db->where('id', $id);
+			         $zapak=$this->db->update('tbl_inventory', $data_update);
+
+			              if($zapak!=0){
+			              redirect("dcadmin/products/view_products","refresh");
+			                      }
+			                      else
+			                      {
+			                        echo "Error";
+			                        exit;
+			                      }
+			            }
+			            if($t=="inactive"){
+			              $data_update = array(
+			           'is_active'=>0
+
+			           );
+
+			           $this->db->where('id', $id);
+			           $zapak=$this->db->update('tbl_inventory', $data_update);
+
+			               if($zapak!=0){
+			               redirect("dcadmin/products/view_products","refresh");
+			                       }
+			                       else
+			                       {
+
+			           $data['e']="Error Occured";
+			                           	// exit;
+			         	$this->load->view('errors/error500admin',$data);
+			                       }
+			            }
+
+
+
+			        }
+			        else{
+
+			            $this->load->view('admin/login/index');
+			        }
+
+			        }
 
 }
--- application/views/admin/products/view_inventory.php
+++ application/views/admin/products/view_inventory.php
@@ -1,21 +1,21 @@
 <div class="content-wrapper">
         <section class="content-header">
            <h1>
-           Products
+           Inventory
           </h1>
           <ol class="breadcrumb">
            <li><a href="<?php echo base_url() ?>admin/dashboard"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li><a href="<?php echo base_url() ?>dcadmin/products/view_products"><i class="fa fa-dashboard"></i> All Products </a></li>
-            <li class="active">View Products</li>
+            <li><a href="<?php echo base_url() ?>dcadmin/products/view_products"><i class="fa fa-dashboard"></i> All Inventory</a></li>
+            <li class="active">View Inventory</li>
           </ol>
         </section>
           <section class="content">
           <div class="row">
              <div class="col-lg-12">
-                 <a class="btn btn-info cticket" href="<?php echo base_url() ?>dcadmin/products/add_products" role="button" style="margin-bottom:12px;"> Add Products</a>
+                 <!-- <a class="btn btn-info cticket" href="<?php echo base_url() ?>dcadmin/products/add_products" role="button" style="margin-bottom:12px;"> Add Products</a> -->
                               <div class="panel panel-default">
                                   <div class="panel-heading">
-                                      <h3 class="panel-title"><i class="fa fa-money fa-fw"></i>View Products</h3>
+                                      <h3 class="panel-title"><i class="fa fa-money fa-fw"></i>View Inventory</h3>
                                   </div>
                                      <div class="panel panel-default">
 
@@ -41,32 +41,20 @@
                                           <thead>
                                               <tr>
                                                   <th>#</th>
-                                                  <th>Image</th>
-                                                  <th>Product Title</th>
-                                                  <th>MRP & Sell Price</th>
-                                                  <th>Model No</th>
-                                                  <th>Inventory</th>
-                                                  <th>Description</th>
+                                                  <th>Stocks</th>
+                                                  <th>Model No.</th>
+                                                  <th>Colour</th>
                                                   <th>Status</th>
                                                   <th>Action</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                  <?php $i=1; foreach($product_data->result() as $data) { ?>
+                                                  <?php $i=1; foreach($inventory_data->result() as $data) { ?>
                         <tr>
                             <td><?php echo $i ?> </td>
-                            <td>
-                                <?php if($data->image!=""){  ?>
-          <img id="slide_img_path" height=50 width=100  src="<?php echo base_url() ?><?php echo $data->image; ?>">
-                            <?php }else {  ?>
-                            Sorry No image Found
-                            <?php } ?>
-                              </td>
-                            <td><?php echo $data->title ?></td>
-                            <td><?php echo $data->mrp." & ".$data->sell_price ?></td>
+                            <td><?php echo $data->stocks ?></td>
                             <td><?php echo $data->model_no ?></td>
-                            <td><a href="<?php echo base_url() ?>dcadmin/products/view_inventory/<?php echo base64_encode($data->id) ?>">Inventory</a></td>
-                            <td><?php echo $data->description ?></td>
+                            <td><?php echo $data->colour ?></td>
 
                               <td><?php if($data->is_active==1){ ?>
         <p class="label bg-green" >Active</p>
@@ -84,11 +72,11 @@
 <ul class="dropdown-menu" role="menu">
 
 <?php if($data->is_active==1){ ?>
-<li><a href="<?php echo base_url() ?>dcadmin/products/updateproductsStatus/<?php echo base64_encode($data->id) ?>/inactive">Inactive</a></li>
+<li><a href="<?php echo base_url() ?>dcadmin/products/updateproductsinventoryStatus/<?php echo base64_encode($data->id) ?>/inactive">Inactive</a></li>
 <?php } else { ?>
-<li><a href="<?php echo base_url() ?>dcadmin/products/updateproductsStatus/<?php echo base64_encode($data->id) ?>/active">Active</a></li>
+<li><a href="<?php echo base_url() ?>dcadmin/products/updateproductsinventoryStatus/<?php echo base64_encode($data->id) ?>/active">Active</a></li>
 <?php		}   ?>
-<li><a href="<?php echo base_url() ?>dcadmin/products/update_products/<?php echo base64_encode($data->id) ?>">Edit</a></li>
+<li><a href="<?php echo base_url() ?>dcadmin/products/update_inventory/<?php echo base64_encode($data->id) ?>">Edit</a></li>
 <li><a href="javascript:;" class="dCnf" mydata="<?php echo $i ?>">Delete</a></li>
 </ul>
 </div>
