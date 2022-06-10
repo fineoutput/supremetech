6dfcb5614db02d262e1041f5a60295317fd7e609
Divyanshu <divyanshu6800@gmail.com>
2022-06-10 12:57:31 +0530

    changes

--- application/views/admin/two_images/view_two_images.php
+++ application/views/admin/two_images/view_two_images.php
@@ -11,7 +11,7 @@
   <section class="content">
     <div class="row">
       <div class="col-lg-12">
-        <a class="btn custom_btn" href="<?php echo base_url() ?>dcadmin/Two_images/add_two_images" role="button" style="margin-bottom:12px;"> Add Two Images</a>
+        <!-- <a class="btn custom_btn" href="<?php echo base_url() ?>dcadmin/Two_images/add_two_images" role="button" style="margin-bottom:12px;"> Add Two Images</a> -->
         <div class="panel panel-default">
           <div class="panel-heading">
             <h3 class="panel-title"><i class="fa fa-money fa-fw"></i>View Two Images</h3>
@@ -101,7 +101,7 @@
                               <?php } ?>
                               <li><a href="<?php echo base_url() ?>dcadmin/Two_images/update_two_images/<?php echo
         base64_encode($data->id) ?>">Edit</a></li>
-                              <li><a href="javascript:;" class="dCnf" mydata="<?php echo $i ?>">Delete</a></li>
+                              <!-- <li><a href="javascript:;" class="dCnf" mydata="<?php echo $i ?>">Delete</a></li> -->
                             </ul>
                           </div>
                         </div>
