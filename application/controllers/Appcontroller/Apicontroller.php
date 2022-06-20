8e5138e8858638e97ce239722ebcba1b862cbe6b
Divyanshu <divyanshu6800@gmail.com>
2022-06-20 17:59:00 +0530

    search api changes

--- application/controllers/Appcontroller/Apicontroller.php
+++ application/controllers/Appcontroller/Apicontroller.php
@@ -2694,6 +2694,7 @@ class Apicontroller extends CI_Controller
                 $this->db->select('*');
                 $this->db->from('tbl_products');
                 $this->db->like('productname', $string);
+                $this->db->or_like('modelno', $string);
                 $this->db->where('is_active', 1);
                 $search_string= $this->db->get();
                 // print_r ($string_check);
